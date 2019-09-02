52722ca1e6469f8c2e58464630c097d62593fa48
Jørgen Juel <jorgen.juel@hotmail.com>
2019-09-02 10:51:52 +0200

    Added tracking code

--- views/partials/head.blade.php
+++ views/partials/head.blade.php
@@ -3,4 +3,15 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   @php wp_head() @endphp
+  @if(get_field('ga', 'options'))
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ get_field('ga', 'options') }}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', {{ get_field('ga', 'options') }});
+</script>
+
+  @endif
 </head>
